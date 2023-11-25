5e1857ebc320487d83a73c5c29ea516c15d416b7
emanthology <151116459+emanthology@users.noreply.github.com>
2023-11-25 09:59:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,458 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>REDLINK BROADBAND WIRELESS ACCESS INC.</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/clients/logo.jpg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+  <script src="script.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    (function () {
+      emailjs.init("bveAlY0S4N1lpA3Js");
+    })();
+  </script>
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><img src="assets/img/logo.png" alt=""></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero"><i class="fa-solid fa-house"
+                style="margin-right: 5px;"></i>Home</a></li>
+          <li class="dropdown"><a href="company/company.html"><i class="fa-solid fa-address-card"
+                style="margin-right: 5px;"></i><span>About Us</span></a>
+          </li>
+          <li class="dropdown"><a href="../redlink/product.html"><i class="fa-brands fa-product-hunt"
+                style="margin-right: 5px;"></i></i><span>Products</span></a>
+          </li>
+          <li>
+          <li class="dropdown"><a href="#services"><i class="fa-solid fa-clone"
+                style="margin-right: 5px;"></i><span>Services</span></a></li>
+
+          <li><a class="nav-link scrollto" href="#contact"><i class="fa-solid fa-address-book"
+                style="margin-right: 5px;"></i>Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
+          data-aos="fade-up" data-aos-delay="200">
+          <h1><span>REDLINK</span> BROADBAND WIRELESS ACCESS INC</h1>
+          <p>
+            REDLINK Broadband Wireless Access Inc. is a fast growing System Integrator that provide E2E Solutions for
+            telecommunications carriers, network operators and corporate. Redlink is the leading provider of broadband
+            wireless access and backhaul products. Its award-winning product enable carriers and service providers to
+            establish fixed and mobile broadband wireless networks to deliver advanced services to business and
+            residential subscribers.
+            <br><br>
+            NOW, we are authorized Distributor of Digital Factory / Process Industries and Drives Division - Process
+            Automation (PA) of Siemens RuggedCom that cater electric and water utility company for its SCADA,
+            distribution and work force. We are the leading SI and dealer of Redline WiMAX and Proprietary Enterprise
+            Broadband Wireless Infrastructure products in the Philippines. We are also in partnership with leading
+            Wireless Technology and Networking Manufacturer.
+          </p>
+
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+              aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <rect width="100%" height="100%" fill="#777" />
+              </svg>
+              <img src="assets/img/banner01.jpg">
+
+            </div>
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <rect width="100%" height="100%" fill="#777" />
+              </svg>
+              <img src="assets/img/banner02.jpg">
+
+            </div>
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <rect width="100%" height="100%" fill="#777" />
+              </svg>
+              <img src="assets/img/banner03.jpg">
+
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+
+
+      </div>
+    </section>
+
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients section-bg">
+      <div class="container">
+
+        <div class="row section-title" data-aos="zoom-in">
+          <h2>OUR CLIENTS</h2>
+          <h2>- LUZON -</h2>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-5.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-7.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-8.png" class="img-fluid" alt="">
+          </div>
+
+        </div>
+
+        <div class="row section-title" data-aos="zoom-in">
+
+          <h2>- VISAYAS -</h2>
+
+          <div class="col-lg-6 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-6 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-9.png" class="img-fluid" alt="">
+          </div>
+        </div>
+
+        <div class="row section-title" data-aos="zoom-in">
+          <h2>- MINDANAO -</h2>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-10.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-11.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-12.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-xl-9 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-13.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </section><!-- End Cliens Section -->
+
+
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>For more inquiries, you can drop us a message and our team will definitely attend to your concerns as soon
+            as possible.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>1371 Crisostomo St., Brgy 518, Sampaloc, Metro Manila, Philippines, 1008</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@redlink.com.ph</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>(+63) 917-529-7005 <br> (02) 7238-5843</p>
+              </div>
+
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.617860713646!2d120.9917226759339!3d14.620831876579343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b6056edcdba9%3A0x9b249bce131e51b0!2sRedlink%20Broadband%20Wireless%20Access%2C%20Inc.!5e0!3m2!1sfil!2sph!4v1684421488764!5m2!1sfil!2sph"
+                width="400" height="290" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade" frameborder="0"
+                style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+
+            <div class="card mt-5">
+              <div class="card-title">
+                <h2 class="text-center py-2"> Contact Us </h2>
+                <hr>
+                <?php
+                $Msg = "";
+                if (isset($_GET['error'])) {
+                  $Msg = " Please Fill in the Blanks ";
+                  echo '<div class="alert alert-danger">' . $Msg . '</div>';
+                }
+                if (isset($_GET['success'])) {
+                  $Msg = " Your Message Has Been Sent ";
+                  echo '<div class="alert alert-success">' . $Msg . '</div>';
+                }
+                ?>
+
+              </div>
+              <div class="card-body">
+                <form action="process.php" method="post">
+                  <input type="text" name="UName" placeholder="User Name" class="form-control mb-2">
+                  <input type="email" name="Email" placeholder="Email" class="form-control mb-2">
+                  <input type="text" name="Subject" placeholder="Subject" class="form-control mb-2">
+                  <textarea name="msg" class="form-control mb-2" placeholder="Message"></textarea>
+                  <button class="btn btn-success" name="btn-send">Send</button>
+                </form>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+        </script>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients section-bg">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+
+          <h2>PRODUCTS</h2>
+
+          <div class="col-lg-4 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/partners/Siemens-logo.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-4 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/partners/aviat.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-4 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/partners/emerson.png" class="img-fluid" alt="">
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+
+    </section><!-- End Cliens Section -->
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Redlink Broadband Wireless Access Inc.</h3>
+            <p>
+              1371 Crisostomo St., Brgy 518, <br>Sampaloc, Metro Manila, <br>Philippines, 1008 <br><br>
+              <strong>Phone:</strong> (+63) 917-529-7005<br>
+              <strong>Email:</strong> info@redlink.com.ph<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Fiber Optic Cable</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Structured Layout Cabling and Other Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Fire Detection Alarm Solution (FDAS)</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Manpower Outsourcing – On Call or SLA</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Project Management Team services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Antenna Tower/Pole Mast Construction and
+                  Installation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Antenna Tower/Pole Mast Rehabilitation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Transmission Tower Repainting, Repairs and
+                  Rehabilitation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Electrical Technical Support intended for Electrical
+                  Utility, Design, and Maintenance</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        <strong><span>REDLINK BROADBAND WIRELESS ACCESS INC</span></strong>. All Rights Reserved
+      </div>
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+
+</body>
+
+</html>
